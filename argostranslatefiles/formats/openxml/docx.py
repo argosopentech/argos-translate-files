82990c5a0dbc1ddbba7008ecab3148d4114654fb
Tommi Nieminen <nieminentommi@hotmail.com>
2023-08-15 23:41:57 +0300

    changed pptx and docx handling to use python-pptx and python-docx instead of beatiful soup, disabled unnecessary file formats

--- argostranslatefiles/formats/openxml/docx.py
+++ argostranslatefiles/formats/openxml/docx.py
@@ -1,8 +1,6 @@
-import zipfile
-
 from argostranslate.tags import translate_tags
 from argostranslate.translate import ITranslation
-from bs4 import BeautifulSoup
+from docx import Document
 
 from argostranslatefiles.formats.abstract_xml import AbstractXml
 
@@ -12,24 +10,17 @@ class Docx(AbstractXml):
 
     def translate(self, underlying_translation: ITranslation, file_path: str):
         outzip_path = self.get_output_path(underlying_translation, file_path)
+        document = Document(file_path)
+        for paragraph in document.paragraphs:
+            if len(paragraph.runs) == 1:
+                paragraph.runs[0].text = underlying_translation.translate(paragraph.runs[0].text)
+            elif len(paragraph.runs) == 0:
+                continue
+            else:
+                first_run_style = paragraph.runs[0].style
+                paragraph.text = underlying_translation.translate(paragraph.text)
+                paragraph.runs[0].style = first_run_style
 
-        inzip = zipfile.ZipFile(file_path, "r")
-        outzip = zipfile.ZipFile(outzip_path, "w")
-
-        for inzipinfo in inzip.infolist():
-            with inzip.open(inzipinfo) as infile:
-                if inzipinfo.filename == "word/document.xml":
-                    soup = BeautifulSoup(infile.read(), 'xml')
-
-                    itag = self.itag_of_soup(soup)
-                    translated_tag = translate_tags(underlying_translation, itag)
-                    translated_soup = self.soup_of_itag(translated_tag)
-
-                    outzip.writestr(inzipinfo.filename, str(translated_soup))
-                else:
-                    outzip.writestr(inzipinfo.filename, infile.read())
-
-        inzip.close()
-        outzip.close()
+        document.save(outzip_path)
 
         return outzip_path
--- argostranslatefiles/formats/openxml/pptx.py
+++ argostranslatefiles/formats/openxml/pptx.py
@@ -1,9 +1,5 @@
-import re
-import zipfile
-
-from argostranslate.tags import translate_tags
 from argostranslate.translate import ITranslation
-from bs4 import BeautifulSoup
+from pptx import Presentation
 
 from argostranslatefiles.formats.abstract_xml import AbstractXml
 
@@ -13,24 +9,16 @@ class Pptx(AbstractXml):
 
     def translate(self, underlying_translation: ITranslation, file_path: str):
         outzip_path = self.get_output_path(underlying_translation, file_path)
+        presentation = Presentation(file_path)
+        
+        for slide in presentation.slides:
+            for shape in slide.shapes:
+                if not shape.has_text_frame:
+                    continue
+                for paragraph in shape.text_frame.paragraphs:
+                    paragraph.text = underlying_translation.translate(paragraph.text)
+       
+        presentation.save(outzip_path)  
 
-        inzip = zipfile.ZipFile(file_path, "r")
-        outzip = zipfile.ZipFile(outzip_path, "w")
-
-        for inzipinfo in inzip.infolist():
-            with inzip.open(inzipinfo) as infile:
-                if re.match(r"ppt\/slides\/slide[0-9]*\.xml", inzipinfo.filename):
-                    soup = BeautifulSoup(infile.read(), 'xml')
-
-                    itag = self.itag_of_soup(soup)
-                    translated_tag = translate_tags(underlying_translation, itag)
-                    translated_soup = self.soup_of_itag(translated_tag)
-
-                    outzip.writestr(inzipinfo.filename, str(translated_soup))
-                else:
-                    outzip.writestr(inzipinfo.filename, infile.read())
-
-        inzip.close()
-        outzip.close()
 
         return outzip_path
