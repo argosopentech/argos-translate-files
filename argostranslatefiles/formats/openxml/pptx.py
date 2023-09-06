3b7af0720c4c1a476408b9c816d136bcc7e49b88
Tommi Nieminen <nieminentommi@hotmail.com>
2023-09-06 21:18:27 +0300

    added table handling to pptx

--- argostranslatefiles/formats/openxml/pptx.py
+++ argostranslatefiles/formats/openxml/pptx.py
@@ -1,23 +1,30 @@
 from argostranslate.translate import ITranslation
 from pptx import Presentation
-
+import sys
 from argostranslatefiles.formats.abstract_xml import AbstractXml
 
 
 class Pptx(AbstractXml):
     supported_file_extensions = ['.pptx']
 
+    def translate_paragraphs(self, paras, underlying_translation):
+        for paragraph in paras:
+            paragraph.text = underlying_translation.translate(paragraph.text)
+        
     def translate(self, underlying_translation: ITranslation, file_path: str):
         outzip_path = self.get_output_path(underlying_translation, file_path)
         presentation = Presentation(file_path)
         
         for slide in presentation.slides:
             for shape in slide.shapes:
-                if not shape.has_text_frame:
-                    continue
-                for paragraph in shape.text_frame.paragraphs:
-                    paragraph.text = underlying_translation.translate(paragraph.text)
-       
+                if shape.has_text_frame:
+                    self.translate_paragraphs(shape.text_frame.paragraphs, underlying_translation)
+            
+                if shape.has_table:
+                    for r in shape.table.rows:
+                        for c in r.cells:
+                            c.text = underlying_translation.translate(c.text)
+
         presentation.save(outzip_path)  
 
 
