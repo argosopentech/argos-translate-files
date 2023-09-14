7c449a66047473c23efe97a535d5bb16d78f6160
Tommi Nieminen <nieminentommi@hotmail.com>
2023-09-14 22:47:04 +0300

    fixed font, table issues with openxml formats

--- argostranslatefiles/formats/openxml/docx.py
+++ argostranslatefiles/formats/openxml/docx.py
@@ -1,25 +1,49 @@
 from argostranslate.tags import translate_tags
 from argostranslate.translate import ITranslation
 from docx import Document
-
+import sys
 from argostranslatefiles.formats.abstract_xml import AbstractXml
 
 
 class Docx(AbstractXml):
     supported_file_extensions = ['.docx']
 
-    def translate(self, underlying_translation: ITranslation, file_path: str):
-        outzip_path = self.get_output_path(underlying_translation, file_path)
-        document = Document(file_path)
-        for paragraph in document.paragraphs:
+    def translate_paragraphs(self,paragraphs,underlying_translation):
+        for paragraph in paragraphs:
+            sys.stderr.write(f'{paragraph.text}\n')
+
             if len(paragraph.runs) == 1:
                 paragraph.runs[0].text = underlying_translation.translate(paragraph.runs[0].text)
             elif len(paragraph.runs) == 0:
                 continue
             else:
                 first_run_style = paragraph.runs[0].style
+                #get the dominant run in the paragraph
+                dominant_run = max(paragraph.runs, key=lambda x: len(x.text))
+                sys.stderr.write(f'dominant run: {dominant_run.text}.\n')
+                dom_font = dominant_run.font.name
+                dom_bold = dominant_run.font.bold
+                dom_italic = dominant_run.font.italic
+                dom_underline = dominant_run.font.underline
+
                 paragraph.text = underlying_translation.translate(paragraph.text)
-                paragraph.runs[0].style = first_run_style
+                paragraph.runs[0].font.name = dom_font
+                paragraph.runs[0].font.bold = dom_bold
+                paragraph.runs[0].font.italic = dom_italic
+                paragraph.runs[0].font.underline = dom_underline
+
+
+
+    def translate(self, underlying_translation: ITranslation, file_path: str):
+        outzip_path = self.get_output_path(underlying_translation, file_path)
+        document = Document(file_path)
+
+        self.translate_paragraphs(document.paragraphs,underlying_translation)
+        
+        for table in document.tables:
+            for row in table.rows:
+                for cell in row.cells:
+                    self.translate_paragraphs(cell.paragraphs,underlying_translation)
 
         document.save(outzip_path)
 
--- argostranslatefiles/formats/openxml/pptx.py
+++ argostranslatefiles/formats/openxml/pptx.py
@@ -9,8 +9,17 @@ class Pptx(AbstractXml):
 
     def translate_paragraphs(self, paras, underlying_translation):
         for paragraph in paras:
-            paragraph.text = underlying_translation.translate(paragraph.text)
-        
+            #get the average font size for the para
+            if paragraph.runs:
+                font_sizes = [(x.font.size,len(x.text)) for x in paragraph.runs]
+                #sys.stderr.write(f"paragraph font size: {paragraph.font.size}. font sizes: {str(font_sizes)}\n")
+                if font_sizes:
+                    #avg_font_size = round(sum([x[0] for x in font_sizes])/len([x[0] for x in font_sizes]))
+                    most_common_font_size = sorted(font_sizes,key=lambda x: x[1])[0]
+                    if most_common_font_size:
+                        paragraph.font.size = most_common_font_size[0]
+                paragraph.text = underlying_translation.translate(paragraph.text)
+         
     def translate(self, underlying_translation: ITranslation, file_path: str):
         outzip_path = self.get_output_path(underlying_translation, file_path)
         presentation = Presentation(file_path)
@@ -21,9 +30,11 @@ class Pptx(AbstractXml):
                     self.translate_paragraphs(shape.text_frame.paragraphs, underlying_translation)
             
                 if shape.has_table:
-                    for r in shape.table.rows:
-                        for c in r.cells:
-                            c.text = underlying_translation.translate(c.text)
+                    for row in shape.table.rows:
+                        for cell in row.cells:
+                            self.translate_paragraphs(cell.text_frame.paragraphs, underlying_translation)
+                            #sys.stderr.write(f"paragraph font size: {paragraph.font.size}. font sizes: {str(font_sizes)}\n")
+                            #c.text = underlying_translation.translate(c.text)
 
         presentation.save(outzip_path)  
 
